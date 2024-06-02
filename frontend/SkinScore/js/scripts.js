4068e500f82b7632acbac0813e35e24c8303070e
Guilherme-Luis <guilherme_silva_luis@hotmail.com>
2024-06-02 20:17:11 -0300

    (02/06) changes about rating system

--- frontend/SkinScore/js/scripts.js
+++ frontend/SkinScore/js/scripts.js
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
         star.addEventListener("click", () => {
-            starNumber = index + 1
+            starNumber = 5 - index
         });
     });
 });
